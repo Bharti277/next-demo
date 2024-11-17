d3b624baae5ce4c2a04bb849f57635386d0b929d
Bharti277 <bhartideependra277@gmail.com>
2024-11-17 14:26:01 +0530

    new changes in payment

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Link from "next/link";
+import Styles from "../styles/Home.module.css";
 
 function HomePage() {
-  return <div>HomePage Checking git commit in branch</div>;
+  return (
+    <div className={Styles.container}>
+      <nav>
+        <ul>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/about">
+            <li>About</li>
+          </Link>
+          <Link href="/blogpost/blog">
+            <li>Blog</li>
+          </Link>
+          <Link href="/contact">
+            <li>Contact</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default HomePage;
